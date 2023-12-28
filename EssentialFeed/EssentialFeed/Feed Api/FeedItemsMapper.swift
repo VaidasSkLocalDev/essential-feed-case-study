4fefe14bc1036e52b08078636ef262813b22c7a8
Vaidas <vaidasskios@gmail.com>
2023-12-28 15:40:18 +0200

    Move FeedItemsMapper  to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Api/FeedItemsMapper.swift
@@ -0,0 +1,39 @@
+//
+//  FeedItemsMapper.swift
+//  EssentialFeed
+//
+//  Created by Vaidas Skardzius on 2023-12-28.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+internal final class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            return FeedItem(id: id,
+                            description: description,
+                            location: location,
+                            imageUrl: image)
+        }
+    }
+    
+    static var OK_200: Int { return 200 }
+    
+    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == OK_200  else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        
+        return try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
+    }
+}
